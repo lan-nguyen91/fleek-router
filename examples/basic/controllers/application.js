d1659929bbc71ddb6e7e230788474c9042356fad
Lan Nguyen <lan.nguyen@hart.com>
2016-05-23 09:04:21 -0700

    implement operation ID logic mapping to controller

--- examples/basic/controllers/application.js
+++ examples/basic/controllers/application.js
@@ -1,3 +1,7 @@
 module.exports.get = function *() {
   this.body = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>foo</title></head><body>Hello World</body></html>'
 }
+
+module.exports.default = function*() {
+  this.body = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>foo</title></head><body>Hello World 2</body></html>'
+}
